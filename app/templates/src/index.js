9f01d85d63fea45b7cbfdd3566da19e8e90ab7c0
Matthew Conlen <mc@mathisonian.com>
2015-07-09 22:47:05 -0400

    fix comment styles

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -2,11 +2,13 @@
 
 var LightningVisualization = require('lightning-visualization');
 
+
 /* 
-// Uncomment this code to require an optional stylesheet
-var fs = require('fs');
-var styles = fs.readFileSync(__dirname + '/test.css');
-*/
+ * Uncomment this code to require an optional stylesheet
+ */
+// var fs = require('fs');
+// var styles = fs.readFileSync(__dirname + '/test.css');
+
 
 /*
  * Extend the base visualization object
@@ -15,39 +17,48 @@ var Visualization = LightningVisualization.extend({
 
     init: function() {
         /*
-        // FILL IN Add any logic for initializing the visualization
-        */
+         * FILL IN Add any logic for initializing the visualization
+         */
         this.render();
     },
 
-    /*styles: styles,*/ // optionally pass a string of CSS styles 
+    /*
+     * optionally pass a string of CSS styles 
+     */
+    // styles: styles,
 
     render: function() {
         /*
-        // FILL IN Render the visualization
-        // FILL IN Get data / selector from this.data and this.selector
-        */
+         * FILL IN Render the visualization
+         */
+
+        /*
+         * FILL IN Get data / selector from this.data and this.selector
+         */
     },
 
     formatData: function(data) {
         /*
-        // Format your data from a raw JSON blob
-        */
+         * Format your data from a raw JSON blob
+         */
         return data;
     },
 
     updateData: function(formattedData) {
         this.data = formattedData;
         /*
-        // FILL IN Re-render your visualization
-        */
+         * FILL IN Re-render your visualization
+         */
     },
 
     appendData: function(formattedData) {    
         /*
-        // FILL IN Update this.data to include the newly formatted data
-        // FILL IN Re-render the visualization
-        */    
+         * FILL IN Update this.data to include the newly formatted data
+         */
+
+        /*
+         * FILL IN Re-render the visualization
+         */    
     }
 
 });
